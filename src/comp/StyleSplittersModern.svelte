ea661bad6a37bbbf888cff8a1c71f74191397884
olivier <orefalo@gmail.com>
2022-05-06 22:19:57 +0400

    update demo code

--- src/comp/StyleSplittersModern.svelte
+++ src/comp/StyleSplittersModern.svelte
@@ -3,60 +3,87 @@
 	import { HighlightSvelte } from 'svelte-highlight';
 
 	let code = `
-<Splitpanes horizontal style="height: 400px">
+    <Splitpanes theme="no-splitter" horizontal style="height: 400px" dblClickSplitter={false}>
+	<Pane size=6 minSize=6 maxSize=6 >
+		<p>MenuBar</p>
+	</Pane>
+	<Pane size=6 minSize=6 maxSize=6>
+		<p>ToolBar</p>
+	</Pane>
 	<Pane>
-		<Splitpanes>
+		<Splitpanes theme="modern-theme">
 			<Pane>
-				<span>1</span>
+				<p>Folder<br/>
+                Try those --></p>
 			</Pane>
 			<Pane>
-				<span>2</span>
+				<p>Views</p>
 			</Pane>
 			<Pane>
-				<span>3</span>
+                <p>Details</p>
 			</Pane>
 		</Splitpanes>
 	</Pane>
-	<Pane
-		><em class="specs">
-			<p>In this example the splitters are thin lines but the reactive touch zone is spread to 30 pixels all around!</p>
-		</em>
+	<Pane size=6 minSize=6 maxSize=6>
+		<p>statusbar</p>
 	</Pane>
 </Splitpanes>
 
 <style global lang="scss">
-	.splitpanes {
-		background-color: #f8f8f8;
-	}
+.splitpanes.modern-theme {
+    .splitpanes__pane {
+        background-color: #f8f8f8;
+    }
+    .splitpanes__splitter {
+        background-color: #ccc;
+        position: relative;
+        &:before {
+            content: '';
+            position: absolute;
+            left: 0;
+            top: 0;
+            transition: opacity 0.4s;
+            background-color: #2db9d2;
+            opacity: 0;
+            z-index: 1;
+        }
+        &:hover:before {
+            opacity: 1;
+        }
+    }
+}
+.modern-theme {
+    &.splitpanes--horizontal > .splitpanes__splitter:before {
+        top: -3px;
+        bottom: -3px;
+        width: 100%;
+    }
+    &.splitpanes--vertical > .splitpanes__splitter:before {
+        left: -3px;
+        right: -3px;
+        height: 100%;
+    }
+}
 
-	.splitpanes__splitter {
-		background-color: #ccc;
-		position: relative;
-	}
-	.splitpanes__splitter:before {
-		content: '';
-		position: absolute;
-		left: 0;
-		top: 0;
-		transition: opacity 0.4s;
-		background-color: rgba(255, 0, 0, 0.3);
-		opacity: 0;
-		z-index: 1;
-	}
-	.splitpanes__splitter:hover:before {
-		opacity: 1;
-	}
-	.splitpanes--vertical > .splitpanes__splitter:before {
-		left: -30px;
-		right: -30px;
-		height: 100%;
-	}
-	.splitpanes--horizontal > .splitpanes__splitter:before {
-		top: -30px;
-		bottom: -30px;
-		width: 100%;
-	}
-</style>
+.splitpanes.no-splitter {
+    .splitpanes__pane {
+        background-color: #f8f8f8;
+    }
+    .splitpanes__splitter {
+        background-color: #ccc;
+        position: relative;
+    }
+}
+.no-splitter {
+    &.splitpanes--horizontal > .splitpanes__splitter:before {
+        width: 0.125rem;
+        pointer-events: none;
+    }
+    &.splitpanes--vertical > .splitpanes__splitter:before {
+        height: 0.125rem;
+        pointer-events: none;
+    }
+}
 `;
 </script>
 
