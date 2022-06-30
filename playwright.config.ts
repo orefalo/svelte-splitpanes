fa32766bc9de81667cce33df385464984548b725
Tal Hadad <tal_hd@hotmail.com>
2022-06-30 12:43:20 +0300

    :truck: chore: make the tests faster by serving the pre-rendered website

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,7 @@ const config: PlaywrightTestConfig = {
 	retries: process.env.CI ? 2 : 0,
 	timeout: 5 * 60 * 1000,
 	webServer: {
-		command: 'npm run preview',
+		command: 'npm run static-serv',
 		port: 3000
 	},
 	use: {
