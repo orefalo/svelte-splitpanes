b92b25cb6c0817fc83476f77cc4ecb2eea5ee59b
Tal Hadad <tal_hd@hotmail.com>
2022-08-02 15:50:29 +0300

    :memo: docs: improve the example \`ProgResize\` to support floating numbers

--- src/comp/ProgResize.svelte
+++ src/comp/ProgResize.svelte
@@ -13,10 +13,10 @@
 <RangeSlider float bind:values={value} min={0} max={100} />
 <Splitpanes class="default-theme" style="height: 400px">
 	<Pane bind:size={value[0]}>
-		<span>{value[0]}%</span>
+		<span>{Math.round(value[0])}%</span>
 	</Pane>
 	<Pane size={100 - value[0]}>
-		<span>{100 - value[0]}%</span>
+		<span>{Math.round(100 - value[0])}%</span>
 	</Pane>
 </Splitpanes>`;
 </script>
@@ -24,15 +24,15 @@
 <h2>Programmatic resizing</h2>
 <p>This example shows the programmatic way of resizing panes with data biding. And how it works both ways.</p>
 
-<RangeSlider float bind:values={value} min={0} max={100} />
+<RangeSlider float bind:values={value} min={0} step={0.01} max={100} />
 <Splitpanes class="default-theme" style="height: 400px">
 	<Pane bind:size={value[0]}>
-		<span>{value[0]}%</span>
+		<span>{Math.round(value[0])}%</span>
 	</Pane>
 	<!-- TODO: I would like if we could have ommited also `size={100 - value[0]}` here,
 		but auto size update isn't working otherwise (yet). -->
 	<Pane size={100 - value[0]}>
-		<span>{100 - value[0]}%</span>
+		<span>{Math.round(100 - value[0])}%</span>
 	</Pane>
 </Splitpanes>
 
