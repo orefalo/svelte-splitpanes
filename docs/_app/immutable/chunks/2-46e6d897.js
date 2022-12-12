d10b74f23c85fcfc72554b99e5f934b53b585c46
orefalo <orefalo@snapcore.com>
2022-12-12 18:20:37 +0000

    Deploying to gh-pages from @ orefalo/svelte-splitpanes@af70fb0fd95e3e39fe692d7846c608338f20f068 🚀

---
+++ docs/_app/immutable/chunks/2-46e6d897.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page-da46b06b.js";import{default as t}from"../components/pages/_page.svelte-947fa05c.js";export{t as component,r as shared};
---
+++ docs/_app/immutable/chunks/3-31e2a519.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page-fca24c55.js";import{default as t}from"../components/pages/test/_page.svelte-075af5e7.js";export{t as component,r as shared};
