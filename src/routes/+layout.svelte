f07ceb6ea419509ec70122ee5af0958376692b62
Tal Hadad <tal_hd@hotmail.com>
2022-12-15 16:21:30 +0200

    :memo: docs: fix icon path

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { base } from '$app/paths';
 	import { page } from '$app/stores';
 
 	import theme from 'svelte-highlight/styles/night-owl';
@@ -62,7 +63,7 @@
 
 	<div class="toc-container">
 		<div role="presentation" class="toc-contents-wrap">
-			<h1 class="toc-head"><img src="/favicon.png" alt="Icon" width="30" height="30" /> Svelte-Splitpane</h1>
+			<h1 class="toc-head"><img src="{base}/favicon.png" alt="Icon" width="30" height="30" /> Svelte-Splitpane</h1>
 			<Contents contents={sections} />
 		</div>
 
