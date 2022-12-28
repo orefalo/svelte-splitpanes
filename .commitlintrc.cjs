e720429090d548a42abc6c1a3fb3db77ca10c5ad
Tal Hadad <tal_hd@hotmail.com>
2022-12-28 23:30:09 +0200

    build: use `commitlint` correctly

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -1,4 +1,7 @@
 module.exports = {
+	extends: [
+        "@commitlint/config-conventional"
+    ],
 	rules: {
 		'header-max-length': [0, 'always', 100],
 		'scope-case': [0, 'always', 'pascal-case']
