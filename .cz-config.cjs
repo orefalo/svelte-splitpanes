06d0331e0b7224072b59947cd8138ff1dbbd08d3
Tal Hadad <tal_hd@hotmail.com>
2022-12-28 22:29:47 +0200

    build: remove the emojies generated in the commit title from \`cz-customizable\` console

--- .cz-config.cjs
+++ .cz-config.cjs
@@ -1,36 +1,36 @@
 module.exports = {
 	types: [
-		{ value: ':sparkles: feat', name: '✨ feat:\tAdding a new feature' },
-		{ value: ':bug: fix', name: '🐛 fix:\tFixing a bug' },
-		{ value: ':memo: docs', name: '📝 docs:\tAdd or update documentation' },
+		{ value: 'feat', name: '✨ feat:\tAdding a new feature' },
+		{ value: 'fix', name: '🐛 fix:\tFixing a bug' },
+		{ value: 'docs', name: '📝 docs:\tAdd or update documentation' },
 		{
-			value: ':lipstick: style',
+			value: 'style',
 			name: '💄 style:\tAdd or update styles, ui or ux'
 		},
 		{
-			value: ':recycle: refactor',
+			value: 'refactor',
 			name: '♻️  refactor:\tCode change that neither fixes a bug nor adds a feature'
 		},
 		{
-			value: ':zap: perf',
+			value: 'perf',
 			name: '⚡️ perf:\tCode change that improves performance'
 		},
 		{
-			value: ':white_check_mark: test',
+			value: 'test',
 			name: '✅ test:\tAdding tests cases'
 		},
 		{
-			value: ':truck: chore',
+			value: 'chore',
 			name: '🚚 chore:\tChanges to the build process or auxiliary tools\n\t\tand libraries such as documentation generation'
 		},
-		{ value: ':rewind: revert', name: '⏪️ revert:\tRevert to a commit' },
-		{ value: ':construction: wip', name: '🚧 wip:\tWork in progress' },
+		{ value: 'revert', name: '⏪️ revert:\tRevert to a commit' },
+		{ value: 'wip', name: '🚧 wip:\tWork in progress' },
 		{
-			value: ':construction_worker: build',
+			value: 'build',
 			name: '👷 build:\tAdd or update regards to build process'
 		},
 		{
-			value: ':green_heart: ci',
+			value: 'ci',
 			name: '💚 ci:\tAdd or update regards to build process'
 		}
 	],
