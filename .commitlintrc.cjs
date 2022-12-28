d30a90a9a2358427665d41db45a7a6a2573d2d50
Olivier Refalo <orefalo@snapcore.com>
2022-12-28 22:21:34 +0400

    :truck: chore: commitlint

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
-	extends: ['gitmoji'],
+	extends: ['./node_modules/commitlint-config-gitmoji'],
 	rules: {
 		'header-max-length': [0, 'always', 100],
 		'scope-case': [0, 'always', 'pascal-case']
 	}
-}
+};
