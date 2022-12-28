3f54dd3b7be73ab3c3e43ed0aa1b4afb573ecbbb
Tal500 <tal_hd@hotmail.com>
2022-12-28 21:40:04 +0000

    Deploying to gh-pages from @ orefalo/svelte-splitpanes@0f0c6c69c7b1ca6a1b81ee25a51aac04c5e551e2 🚀

--- docs/_app/immutable/chunks/11-aecb16b3.js
+++ docs/_app/immutable/chunks/11-aecb16b3.js
@@ -1,1 +1,1 @@
-import{_ as r}from"./_page-7c799ff2.js";import{default as t}from"../components/pages/examples/reordering-splitters/_page.svelte-e3898edc.js";export{t as component,r as universal};
+import{_ as r}from"./_page-8f3d95e9.js";import{default as t}from"../components/pages/examples/reordering-splitters/_page.svelte-e3898edc.js";export{t as component,r as universal};
--- docs/_app/immutable/chunks/7-37e4023b.js
+++ docs/_app/immutable/chunks/7-37e4023b.js
@@ -1,1 +1,1 @@
-import{_ as r}from"./_page-b95cbc4a.js";import{default as t}from"../components/pages/examples/lock-layout/_page.svelte-1723b067.js";export{t as component,r as universal};
+import{_ as r}from"./_page-2a9d3cfa.js";import{default as t}from"../components/pages/examples/lock-layout/_page.svelte-1723b067.js";export{t as component,r as universal};
