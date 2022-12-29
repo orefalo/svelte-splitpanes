4d333020e17b38597c6ff8c4a47cd5fee3cdaacf
Tal500 <tal_hd@hotmail.com>
2022-12-29 16:02:19 +0000

    Deploying to gh-pages from @ orefalo/svelte-splitpanes@94736e943a743a55c0f72f00ce5696648b96a8bf 🚀

--- docs/_app/immutable/chunks/14-1c8559fa.js
+++ docs/_app/immutable/chunks/14-1c8559fa.js
@@ -1,1 +1,1 @@
-import{_ as r}from"./_page-56160fd5.js";import{default as t}from"../components/pages/examples/snap/simple/_page.svelte-6abc2e98.js";export{t as component,r as universal};
+import{_ as r}from"./_page-8f0fe832.js";import{default as t}from"../components/pages/examples/snap/simple/_page.svelte-6abc2e98.js";export{t as component,r as universal};
