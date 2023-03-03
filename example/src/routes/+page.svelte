b0aa467f1d85554b3a9560647d0895a7b482d2d9
Tal Hadad <tal_hd@hotmail.com>
2023-03-03 16:50:13 +0200

    docs: fix url in the example of SvelteKit

--- example/src/routes/+page.svelte
+++ example/src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import { Pane, Splitpanes } from 'svelte-splitpanes';
+
+	const url = 'https://orefalo.github.io/svelte-splitpanes/';
 </script>
 
 <h1>Welcome to Svelte-Splitpanes Minimal Demo!</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation and for more examples.</p>
+<p>Visit <a href={url}>{url}</a> to read the documentation and for more examples.</p>
 
 <Splitpanes style="height: 400px;">
 	<Pane>First Pane</Pane>
