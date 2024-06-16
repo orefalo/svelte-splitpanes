9f7ec2e317594b4079ccb39a5c1292fb3c705bba
Olivier Refalo <orefalo@snapcore.com>
2024-06-16 17:55:45 +0400

    chore: release 8.0.5
    Release-As: 8.0.5

---
+++ src/type_decl.d.ts
@@ -0,0 +1,7 @@
+// fixes a svelte-check issue
+declare module 'svelte-range-slider-pips'; // No chance to augment it at all
+
+declare module 'svelte-highlight/styles/night-owl' {
+	const nightOwl: string;
+	export default nightOwl;
+}
